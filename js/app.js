13040e0ca07cc10d86d24cb372e188ff6fb14bf9
Alvin <alvinramoutar@gmail.com>
2018-07-30 11:15:44 -0400

    Draft 1 Complete. Implemented handlebar for footer, as well as footer navigation functionality.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 var donePageInit = false;
 var jsonData = null;
 
+var pageIDs = ["firstRoboticsCompetition",
+    "purposeOfGuide",
+    "roboRIO",
+    "pdp",
+    "pcm",
+    "vrm",
+    "motorControllers",
+    "otherNotables",
+    "conclusion"];
+var pageTitles = ["Home",
+    "About Guide",
+    "NI roboRIO",
+    "Power Distribution Panel",
+    "Pneumatics Control Module",
+    "Voltage Regulator Module",
+     "Motor Controllers",
+    "Other Notables",
+    "Conclusion"];
+
 // Initialize
 $(document).on("pageinit", function () {
     if (!donePageInit) {
@@ -8,17 +27,38 @@ $(document).on("pageinit", function () {
             console.log(data);
             jsonData = data;
             initQuestionGenerator();
-        });
+            console.log("Done loading questions");
 
-        // If jsonData object exists in localstorage, then there are saved answers.
-        //  Load em' up.
-        let userAnswers = localStorage['FRCQuickstartGuide_QuizData'];
-        if(userAnswers)
-            initLoadUserAnswers(JSON.parse(userAnswers));
+            // If jsonData object exists in localstorage, then there are saved answers.
+            //  Load em' up.
+            let userAnswers = localStorage['FRCQuickstartGuide_QuizData'];
+            if(userAnswers) {
+                initLoadUserAnswers(JSON.parse(userAnswers));
+                console.log("Done loading saved question data");
+            }
+
+            // Add clear saved answers functionality to knowledge quiz 'clear' buttons
+            $("a.clearAllAnswers").on("click", function(e) {
+                localStorage['FRCQuickstartGuide_QuizData'] = JSON.stringify(jsonData);
+                location.reload(true);
+            });
+        });
 
         donePageInit = true;
     }
 });
+
+$(document).on("pagechange", function() {
+    initFooterNavbar(pageIDs.indexOf($.mobile.activePage.attr("id")));
+    $("select[name=select-page]").change(function(e) {
+        console.log($("select[name=select-page] option:selected").text());
+        let newPageID = pageIDs[pageTitles.indexOf($("select[name=select-page] option:selected").text())];
+        //$("select[name=select-page] option:selected").val([]);
+        $("." + $.mobile.activePage.attr("id") + "_pageSelector").val([]);
+        $.mobile.navigate("#" + newPageID, {transition : "flip"});
+    });
+});
+
 
 function initQuestionGenerator() {
     questionGenerator("roboRIO");
@@ -103,10 +143,13 @@ function initLoadUserAnswers(uA) {
     loadUserAnswers(uA, "pcm");
     loadUserAnswers(uA, "vrm");
     loadUserAnswers(uA, "motorControllers");
+
+    // Due to jQuery mobile styling, updates to radio groups aren't done on attribute change.
+    // Thus, manual refresh of radio groups.
+    $("input[type='radio']").checkboxradio("refresh");
 }
 
 function loadUserAnswers(uA, category) {
-
     // Iterate through the quiz block on each page
     for(let i = 0; i < uA[category].length; i++) {
 
@@ -117,14 +160,43 @@ function loadUserAnswers(uA, category) {
             $("label:contains(" + uA[category][i].userAnswer[0] + ")").next().prop("checked", true);
             $("label:contains(" + uA[category][i].userAnswer[0] + ")").trigger('create');
         } else if (uA[category][i].type == "fillin") {
-            console.log(uA[category][i].userAnswer[0]);
-            $("[name=fitb_" + category + "]").val(uA[category][i].userAnswer[0]);
+            $("input[name=fitb_" + category + "]").val(uA[category][i].userAnswer[0]);
         } else if (uA[category][i].type == "diagram") {
             $("[name$=_diag_" + category + "]").each(function(e) {
-                jsonData[category][i].userAnswer.push(
-                    $(this).val()
-                );
+                $(this).val(uA[category][i].userAnswer[e]);
             });
         }
     }
+}
+
+
+function initFooterNavbar(i) {
+    console.log("Initializing footers");
+    let footer = Handlebars.compile($("#footer_template").html());
+    let navHandlebarDataObj = {};
+
+    if(i == 0) {
+        navHandlebarDataObj["prevID"] = pageIDs[i];
+        navHandlebarDataObj["prevTitle"] = pageTitles[i];
+        navHandlebarDataObj["nextID"] = pageIDs[i + 1];
+        navHandlebarDataObj["nextTitle"] = pageTitles[i + 1];
+    } else if(i == pageIDs.length - 1) {
+        navHandlebarDataObj["prevID"] = pageIDs[i - 1];
+        navHandlebarDataObj["prevTitle"] = pageTitles[i - 1];
+        navHandlebarDataObj["nextID"] = pageIDs[i];
+        navHandlebarDataObj["nextTitle"] = pageTitles[i];
+    } else {
+        navHandlebarDataObj["prevID"] = pageIDs[i - 1];
+        navHandlebarDataObj["prevTitle"] = pageTitles[i - 1];
+        navHandlebarDataObj["nextID"] = pageIDs[i + 1];
+        navHandlebarDataObj["nextTitle"] = pageTitles[i + 1];
+    }
+
+    navHandlebarDataObj["currentID"] = pageIDs[i];
+    navHandlebarDataObj["currentTitle"] = pageTitles[i];
+
+    navHandlebarDataObj["pageIDs"] = pageIDs;
+    navHandlebarDataObj["pageTitles"] = pageTitles;
+
+    $("footer[name=" + pageIDs[i] + "]").html(footer(navHandlebarDataObj)).trigger('create');
 }
